2030a07d88faca3a45b6afa81d42f596119e8777
Ananad Shukla <anands2807@gmail.com>
2024-05-12 12:47:08 +0530

    initial

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGODB_LOCAL_URI as string).then(()=>{console.log('mongodb database connected successfully.')}).catch(e=>console.log('Error connecting to mongodb >>> ', e));
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use(cors());
+
+app.get("/api/test", async(req: Request, res: Response)=>{
+  return res.json({message: "test route"})
+})
+
+app.listen(7000, ()=>{
+  console.log("Server listening at 7000.")
+})
