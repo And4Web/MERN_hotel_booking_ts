0b6855398b275b8addad14ef59ff94d0f940bdea
Ananad Shukla <anands2807@gmail.com>
2024-05-16 19:02:34 +0530

    changed .gitignore

--- backend/dist/utils/tryCatch.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tryCatch = (controllerFunc) => {
-    return async (req, res, next) => {
-        await Promise.resolve(controllerFunc(req, res, next)).catch(err => next(err));
-    };
-};
-exports.default = tryCatch;
