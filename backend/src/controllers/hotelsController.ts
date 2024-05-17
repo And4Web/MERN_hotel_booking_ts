04b4742823b7df4399e8f20a17e0b81d027017e7
Ananad Shukla <anands2807@gmail.com>
2024-05-17 15:08:06 +0530

    in hotelsController.ts, took the image buffer from request files array and created a dataURI to upload on cloudinary. go the uploade images urls.

--- backend/src/controllers/hotelsController.ts
+++ backend/src/controllers/hotelsController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import cloudinary from 'cloudinary';
 
 
 // @function - create a hotel 
@@ -8,11 +9,24 @@ export const createHotel = async (req: Request, res: Response) => {
   try {
     const imageFiles = req.files as Express.Multer.File[];
     const newHotel = req.body;
-
+    // console.log("imageFiles >>> ", imageFiles);
     // 1. upload images to cloudinary
+    const uploadPromises = imageFiles.map(async (image) => {
+      const b64 = Buffer.from(image.buffer).toString("base64");
+      
+      let dataURI = "data:" + image.mimetype + ";base64," + b64;
+      
+      const res = await cloudinary.v2.uploader.upload(dataURI, {folder: "hotel-booking"});
+
+      return res.url;
+    })
+    const imageUrls = await Promise.all(uploadPromises);
+    // console.log("image >>> ", imageUrls);
     // 2. if upload successfull add the URLs to new hotel
+    
     // 3. save the new hotel in database
     // 4. return a 201 status
+    return res.status(201).json({})
 
   } catch (error) {
     console.log("Error creating new hotel >>> ", error);
