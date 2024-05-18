7676ccd643cdb01581e5541cea86cd7237a8e697
Ananad Shukla <anands2807@gmail.com>
2024-05-18 12:22:43 +0530

    TypeSection.tsx added in ManageHotelForm.tsx

--- frontend/src/forms/manageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/manageHotelForm/ManageHotelForm.tsx
@@ -1,13 +1,15 @@
 import { FormProvider, useForm } from "react-hook-form"
 import { HotelFormDataType } from "../../types";
 import DetailsSection from "./DetailsSection";
+import TypeSection from "./TypeSection";
 
 function ManageHotelForm() {
   const formMethods = useForm<HotelFormDataType>();
   return (
     <FormProvider {...formMethods}>
-      <form>
+      <form className="flex flex-col gap-10">
         <DetailsSection/>
+        <TypeSection/>
       </form>
     </FormProvider>
   )
---
+++ frontend/src/forms/manageHotelForm/TypeSection.tsx
@@ -0,0 +1,30 @@
+import { useFormContext } from 'react-hook-form';
+import {hotelTypes} from '../../config/hotel-types-config';
+import { HotelFormDataType } from '../../types';
+
+function TypeSection() {
+  const {register, watch, formState: {errors}} = useFormContext<HotelFormDataType>();
+  const typeWatch = watch("type");
+  return (
+    <div className=''>
+      <h2 className="text-2xl font-bold mb-3">Type</h2>
+      <div className="grid grid-cols-5 gap-2">
+        {
+          hotelTypes.map((type)=>(
+            <label className={
+              typeWatch === type ? "cursor-pointer bg-blue-300 text-sm text-blue-900 rounded-full px-4 py-2 font-semibold" : "cursor-pointer bg-gray-300 text-sm rounded-full px-4 py-2 font-semibold"
+            }>
+              <input type="radio" value={type} {...register("type", {required: "Type is required."})} className="hidden"/>
+              <span>{type}</span>
+            </label>
+          ))
+        }
+      </div>
+      {errors.type && (
+        <span className='text-red-500 text-sm font-bold'>{errors.type.message}</span>
+      )}
+    </div>
+  )
+}
+
+export default TypeSection
