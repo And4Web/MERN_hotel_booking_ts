c86cea218c1c9dee992c8b47bb4416479902b1a1
Ananad Shukla <anands2807@gmail.com>
2024-05-20 11:49:24 +0530

    frontend typescript configuration modified

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -2,6 +2,7 @@ import { LoginFormDataType, RegisterFormDataType } from "./types";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL as string || "";
 
+
 export const register = async(formData: RegisterFormDataType)=>{
   const response = await fetch(`${API_BASE_URL}/v1/users/register`, {
     method: "POST",
